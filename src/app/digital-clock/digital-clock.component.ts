2ba4c88bdd1ff7f03cc2293ca42f048fb639978e
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2021-07-18 22:42:19 +0300

    initial-version

--- src/app/digital-clock/digital-clock.component.ts
+++ src/app/digital-clock/digital-clock.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-digital-clock',
   templateUrl: './digital-clock.component.html',
-  styleUrls: ['./digital-clock.component.css']
+  styleUrls: ['./digital-clock.component.css'],
 })
 export class DigitalClockComponent implements OnInit {
+  private initDate: Date = new Date();
+  public hours?: number;
+  public minutes?: number;
+  public seconds?: number;
+  public ampm: string | null = null;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    setInterval(() => this.renewDate(new Date()), 1000);
   }
 
+  private renewDate(date: Date) {
+    this.hours = date.getHours();
+    this.minutes = date.getMinutes();
+    this.seconds = date.getSeconds();
+  }
 }
